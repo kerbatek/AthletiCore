cfe94f6b171b3faa623e82db3a09db89488e6d5f
kerbatek <mateusz.rem2005@gmail.com>
2025-03-06 18:20:47 +0100

    Initial ASP.NET Core Web API project

---
+++ backend/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AthletiCore.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/healthcheck")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                timestamp = DateTime.Now,
+            });
+        }
+    }
+}
